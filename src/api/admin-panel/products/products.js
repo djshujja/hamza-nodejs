716e6d6208dc1cf3331aeb5e85ea99d38542bfe6
djshujja <djshujja@gmail.com>
2020-10-03 14:29:13 +0500

    changed cars model

--- src/api/admin-panel/products/products.js
+++ src/api/admin-panel/products/products.js
@@ -2,105 +2,117 @@ const route = require("express").Router();
 const fs = require("fs");
 const path = require("path");
 const {
-    loadAllProducts,
-    AddProducts,
-    deleteProduct,
-    editSpecificProduct,
-    UpdateProducts,
-    loadSpecificProduct
-} = require('../../../services/products');
+  loadAllProducts,
+  AddProducts,
+  deleteProduct,
+  editSpecificProduct,
+  UpdateProducts,
+  loadSpecificProduct,
+} = require("../../../services/products");
 
-const {uploadProductFile,uploadMultipleFiles,deleteMultipleFiles} = require("../../../assets/utils/uploadFiles");
- 
+const {
+  uploadProductFile,
+  uploadMultipleFiles,
+  deleteMultipleFiles,
+} = require("../../../assets/utils/uploadFiles");
 
+route.post("/new-product-detail", async (req, res, next) => {
+  //console.log(req.files);
 
+  // for(let i = 0; i < req.body.files.length; i++){
+  //     console.log(req.body.files[i]);
+  // }
 
-
-
-
-route.post("/new-product-detail",async (req,res,next)=>{
-    //console.log(req.files);
-
-
-    // for(let i = 0; i < req.body.files.length; i++){
-    //     console.log(req.body.files[i]);
-    // }
- 
-     uploadMultipleFiles(req.files,__dirname+"/../../../assets/images/uploaded/",(uploadedImagePath,uploadMultipleImagePath)=>{
-
-        AddProducts(req.body,uploadedImagePath,uploadMultipleImagePath).then(()=>{
-            res.send("{result:successful,reason:value inserted successfully}")
-        }).catch(err=>{
-           next(err);
+  uploadMultipleFiles(
+    req.files,
+    __dirname + "/../../../assets/images/uploaded/",
+    (uploadedImagePath, uploadMultipleImagePath) => {
+      AddProducts(req.body, uploadedImagePath, uploadMultipleImagePath)
+        .then(() => {
+          res.send("{result:successful,reason:value inserted successfully}");
         })
-    });
-
-   
-   
-   
-   
-
-})
-
-route.get("/load-all-products",async (req,res)=>{
-    loadAllProducts().then(data=>{
-         res.json(JSON.stringify(data));
-    }).catch(err=>{
-         res.json(JSON.stringify(err));
-    });
-});
-
-route.get("/delete-product/:id",async (req,res)=>{
-    deleteProduct(req.params.id)
-    .then(()=>{
-       res.send("data deleted successfully");
-    }).catch(err=>{
-         res.send("error occur "+err);
-    })
-})
-
-route.get("/edit-specific-product/:id",async (req,res)=>{
-    editSpecificProduct(req.params.id).then(data=>{
-         res.json(JSON.stringify(data));
-    }).catch(err=>{
-        res.json(JSON.stringify(err));
-    })
-})
-
-route.post("/update-products/:id",async (req,res,next)=>{
-
-
-   if(req.files){
-
-       uploadMultipleFiles(req.files,__dirname+"/../../../assets/images/uploaded/",(uploadedImagePath,uploadMultipleImagePath)=>{
-               deleteMultipleFiles(req.body.multiple_image_path);
-               updateFile(req,res,next,uploadedImagePath,uploadMultipleImagePath);
-
+        .catch((err) => {
+          next(err);
         });
-   }else{
-       updateFile(req,res,next);
-   }
-
-
+    }
+  );
 });
 
-route.get("/load-specific-product/:id",async (req,res,next)=>{
-    loadSpecificProduct(req.params.id).then(data=>{
-        res.send(data);
-    }).catch(err=>{
-        next(err);
+route.get("/load-all-products", async (req, res) => {
+  loadAllProducts()
+    .then((data) => {
+      res.json(JSON.stringify(data));
+    })
+    .catch((err) => {
+      res.json(JSON.stringify(err));
     });
 });
 
-const updateFile = (req,res,next,imgPath,multipleImagePath)=>{
-    UpdateProducts(req.params.id,req.body,imgPath,multipleImagePath).then(data=>{
-         res.json(JSON.stringify(data));
-    }).catch(err=>{
-        res.json(JSON.stringify(err));
+route.get("/delete-product/:id", async (req, res) => {
+  deleteProduct(req.params.id)
+    .then(() => {
+      res.send("data deleted successfully");
     })
-}
+    .catch((err) => {
+      res.send("error occur " + err);
+    });
+});
 
+route.get("/edit-specific-product/:id", async (req, res) => {
+  editSpecificProduct(req.params.id)
+    .then((data) => {
+      res.json(JSON.stringify(data));
+    })
+    .catch((err) => {
+      res.json(JSON.stringify(err));
+    });
+});
 
+route.post("/update-products/:id", async (req, res, next) => {
+  if (req.files) {
+    uploadMultipleFiles(
+      req.files,
+      __dirname + "/../../../assets/images/uploaded/",
+      (uploadedImagePath, uploadMultipleImagePath) => {
+        deleteMultipleFiles(req.body.multiple_image_path);
+        updateFile(req, res, next, uploadedImagePath, uploadMultipleImagePath);
+      }
+    );
+  } else {
+    updateFile(req, res, next);
+  }
+});
 
+route.get("/load-specific-product/:id", async (req, res, next) => {
+  loadSpecificProduct(req.params.id)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+route.post("/status/:id", async (req, res) => {
+  await loadSpecificProduct(req.params.id).then((data) => {
+    const oldStatus = data.status;
+    data.status = req.body.status;
+    data.save();
+    res.send({
+      data: data,
+      message: `Status of car ${data.name} has been changed from ${oldStatus} to ${data.status}`,
+    });
+  });
+});
+
+const updateFile = (req, res, next, imgPath, multipleImagePath) => {
+  UpdateProducts(req.params.id, req.body, imgPath, multipleImagePath)
+    .then((data) => {
+      res.json(JSON.stringify(data));
+    })
+    .catch((err) => {
+      res.json(JSON.stringify(err));
+    });
+};
 
 module.exports = route;
--- src/models/schemas/addProductsSchemas.js
+++ src/models/schemas/addProductsSchemas.js
@@ -1,120 +1,120 @@
-const {mongoose,MongooseSchema}  = require("../../server/connection");
-
+const { mongoose, MongooseSchema } = require("../../server/connection");
 
 const AddProductsSchemas = new MongooseSchema({
-    name:{
-        type:String,
+  name: {
+    type: String,
+  },
 
+  location: {
+    type: String,
+  },
 
-    },
+  descriptions: {
+    type: String,
+  },
+  model: {
+    type: String,
+  },
 
-    location:{
-        type:String,
+  imagePath: {
+    type: String,
+  },
+  time: {
+    type: Date,
+    default: Date.now,
+  },
+  // rental period
+  // rental_period:{
+  //   type:String,
+  // },
 
-    },
+  daily_cost: {
+    type: String,
+  },
 
+  weekly_cost: {
+    type: String,
+  },
 
-    descriptions:{
-        type:String,
+  monthly_cost: {
+    type: String,
+  },
 
-    },
-    model:{
-        type:String,
-
-    },
-
-    imagePath:{
-        type:String,
-
-    },
-    time : { 
-        type : Date,
-        default: Date.now 
-    },
-    // rental period
-    // rental_period:{
-    //   type:String,
-    // },
-
-
-    daily_cost:{
-      type:String,
-    },
-
-    weekly_cost:{
-        type:String,
-      },
-
-    monthly_cost:{
-        type:String,
-      },
-
-
-    daily_mileage_limit:{
-        type:String,
-    },
-    weekly_mileage_limit:{
-        type:String,
-    },
-    monthly_mileage_limit:{
-        type:String,
-    },
-    color_available:{
-        type:String,
-    },
-    additional_mileage:{
-         type:String,
-    },
-    toll_charge:{
-        type:String,
-    },
-    excess_claim:{
-        type:String,
-    },
-    security_deposit:{
-        type:String,
-    },
-    pickup_charge:{
-        type:String,
-    },
-    accepted_in:{
-        type:String,
-    },
-//    car specification
-    specs_description:{
-        type:String,
-    },
-    //car features
-    features_description:{
-        type:String,
-    },
-    car_rental_company_id:{
-        type:String,
-    },
-    car_rental_company_name:{
-        type:String,
-    },
-    car_type:{
-        type:String,
-    },
-    multiple_image_path:{
-        type:String,
-    },
-    showroom: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: false,
-        ref: "Showroom",
-      },
-
-
-
-
+  daily_mileage_limit: {
+    type: String,
+  },
+  weekly_mileage_limit: {
+    type: String,
+  },
+  monthly_mileage_limit: {
+    type: String,
+  },
+  color_available: {
+    type: String,
+  },
+  additional_mileage: {
+    type: String,
+  },
+  toll_charge: {
+    type: String,
+  },
+  excess_claim: {
+    type: String,
+  },
+  security_deposit: {
+    type: String,
+  },
+  pickup_charge: {
+    type: String,
+  },
+  accepted_in: {
+    type: String,
+  },
+  //    car specification
+  specs_description: {
+    type: String,
+  },
+  //car features
+  features_description: {
+    type: String,
+  },
+  car_rental_company_id: {
+    type: String,
+  },
+  car_rental_company_name: {
+    type: String,
+  },
+  car_type: {
+    type: String,
+  },
+  multiple_image_path: {
+    type: String,
+  },
+  showroom: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: false,
+    ref: "Showroom",
+  },
+  doors: {
+    type: String,
+  },
+  transmission: {
+    type: String,
+  },
+  engine: {
+    type: String,
+  },
+  terms: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ["active", "pause"],
+    default: "active",
+  },
 });
 
-
 module.exports = {
-    AddProductsSchemas,
-    mongoose
-}
-
-
+  AddProductsSchemas,
+  mongoose,
+};
